9bc23de76c1a8709b5d067508da7affabf2c7839
Nomto <disma785@gmail.com>
2026-09-21 11:19:38 +0900

    refactor

--- Week01/app/src/main/java/com/example/dmyapplication/MainActivity.java
+++ Week01/app/src/main/java/com/example/dmyapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.dmyapplication;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +19,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView imageView = findViewById(R.id.pinwheel);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "rotation", 360);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
+
+        Button button1 = findViewById(R.id.start);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animator.start();
+            }
+        });
+        Button button2 = findViewById(R.id.stop);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animator.cancel();
+            }
         });
     }
 }
